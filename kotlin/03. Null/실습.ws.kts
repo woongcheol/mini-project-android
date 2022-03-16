dbc4bfe48df3e80a8018e75db11877b1ed646cad
Woongcheol Shin <1994_100@naver.com>
2022-03-16 17:35:24 +0900

    add practice

---
+++ kotlin/03. Null/실습.ws.kts
@@ -0,0 +1,13 @@
+// Null 작성
+val number: Int? = null
+// val number2: Int = null 불가
+
+// Null 타입에 할당 시 주의 - Null 대비 할당 제한
+val number1: Int? = 10
+val number2: Int? = 3 + 5
+// val number3: Int? = number1 + number2 오류 발생
+// println(number3) 오류 발생
+
+// Null 타입에 할당 시 제한사항 해제
+val num3: Int? = number1!! + number2!!
+println(num3)
