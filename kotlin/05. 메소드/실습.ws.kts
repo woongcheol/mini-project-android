a3b6473c42a7e8c6aea2aa13b1c2a195815942d0
Woongcheol Shin <1994_100@naver.com>
2022-03-16 18:44:42 +0900

    add practice

--- kotlin/05. 메소드/실습.ws.kts
+++ kotlin/05. 메소드/실습.ws.kts
@@ -1,13 +1,34 @@
+// 함수 선언
 fun hello_world(a: Int, b: Int): Int {
     var c = a + b
     return c
 }
 
-println(hello_world(1, 2))
+//println(hello_world(1, 2))
 
 fun hello_world2(a: Int, b: Int): Unit {
     var c = a + b
     println(c)
 }
 
-println(hello_world2(1, 2))
+//println(hello_world2(1, 2))
+
+fun plusNumber(firstNum: Int, secondNum: Int): Int {
+    val result: Int = firstNum + secondNum
+    return result
+}
+
+// 함수를 호출하는 방법
+plusNumber(firstNum = 10, secondNum = 20)
+plusNumber(10, 20)
+
+// 기본값이 있는 함수 선언
+fun plusNumberWithDefault(firstNum: Int, secondNum: Int = 10): Int {
+    return firstNum + secondNum
+}
+println(plusNumberWithDefault(10))
+println(plusNumberWithDefault(10, 20))
+println(plusNumberWithDefault(firstNum = 10))
+//print(plusNumberWithDefault(10, secondNum = 10))
+
+
