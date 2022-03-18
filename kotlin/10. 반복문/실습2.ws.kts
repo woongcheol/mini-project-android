e897ceac448a6897fa9da85a022d6c141d3321b1
Woongcheol Shin <1994_100@naver.com>
2022-03-18 23:17:31 +0900

    add practice

---
+++ kotlin/10. 반복문/실습2.ws.kts
@@ -0,0 +1,18 @@
+// While
+
+var i = 0
+while (i < 5) {
+    if (i % 2 == 0){
+        println("짝수")
+    } else {
+        println("홀수")
+    }
+    ++i
+}
+
+// DO-While
+var z = 10
+do {
+    println("시작")
+    z++
+} while(z < 20)
