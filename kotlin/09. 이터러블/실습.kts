6ecd3ed5463bdd4c0432181181117a39b9375030
Woongcheol Shin <1994_100@naver.com>
2022-03-18 22:44:07 +0900

    add practice

--- kotlin/09. 이터러블/실습.kts
+++ kotlin/09. 이터러블/실습.kts
@@ -1,3 +1,19 @@
 // Iterable 반복
+
+// Range
+val range1 = 1..10
+val range2 = 1 until 10
+val range3 = 'A'..'Z'
+println(range1)
+println(range2)
+println(range3)
+
 // Progression
-// 닫힌 구간, 열린 구간
+val range4 = 1..10 step 2
+println(range4)
+val range5 = 10 downTo 1 step 2
+println(range5)
+
+// Collection
+val collection1 = listOf<Int>(1, 2, 3, 4, 5)
+
