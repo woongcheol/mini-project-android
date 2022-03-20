aad01810b307b2ae9b08ae9c8ee25a2017ec453e
Woongcheol Shin <1994_100@naver.com>
2022-03-21 04:12:00 +0900

    add practice

--- kotlin/14. 클래스/실습3.ws.kts
+++ kotlin/14. 클래스/실습3.ws.kts
@@ -1,3 +1,15 @@
 // getter, setter
-
+class Book() {
+    var title : String = "모름"
+        get() {
+            return field
+        }
+        set(value) {
+            field = value
+        }
+}
+val book = Book()
+println(book.title)
+book.title = "제목없음"
+println(book.title)
 // lateinit
