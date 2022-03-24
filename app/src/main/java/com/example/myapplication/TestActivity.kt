1fb1f0a0be0735cebbbbaea9d485d30bb6ac3108
Woongcheol Shin <1994_100@naver.com>
2022-03-25 06:58:43 +0900

    add practice

--- app/src/main/java/com/example/myapplication/TestActivity.kt
+++ app/src/main/java/com/example/myapplication/TestActivity.kt
@@ -2,10 +2,18 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
 
 class TestActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_test)
+
+        val textViewOne : TextView = findViewById(R.id.text1)
+        val buttonOne : Button = findViewById(R.id.button1)
+
+        Log.d("testt", textViewOne.text.toString())
     }
 }
