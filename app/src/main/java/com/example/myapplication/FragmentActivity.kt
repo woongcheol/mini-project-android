c7b0fe9424f5e797da9c28639fead7d568047ee1
Woongcheol Shin <1994_100@naver.com>
2022-03-28 04:48:25 +0900

    add practice(fragment)

--- app/src/main/java/com/example/myapplication/FragmentActivity.kt
+++ app/src/main/java/com/example/myapplication/FragmentActivity.kt
@@ -2,6 +2,7 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
 
 class FragmentActivity : AppCompatActivity() {
@@ -17,17 +18,43 @@ class FragmentActivity : AppCompatActivity() {
         val fragmentFirst = FragmentFirst()
         (findViewById<TextView>(R.id.add)).setOnClickListener {
             val transaction = fragmentmanager.beginTransaction() // 시작
-            transaction.replace(R.id.root, fragmentFirst)
+            // fragment에 데이터를 전달하는 방법
+            val bundle = Bundle()
+            bundle.putString("key", "hello")
+            fragmentFirst.arguments = bundle
+
+
+            transaction.replace(R.id.root, fragmentFirst, "fragment_first_tag")
             transaction.commit() // 끝
         }
         (findViewById<TextView>(R.id.remove)).setOnClickListener {
-
+            val transaction = fragmentmanager.beginTransaction()
+            transaction.remove(fragmentFirst)
+            transaction.commit()
         }
 
+        (findViewById<TextView>(R.id.access_fragment)).setOnClickListener {
+            // XML에 있는 fragment를 찾는 방법
+//            val fragmentFirst =
+//                supportFragmentManager.findFragmentById(R.id.fragment_first) as FragmentFirst
+//
+//            fragmentFirst.printTestLog()
+
+            // XML에 없는 fragment를 찾는 방법
+            val fragmentFirst =
+                supportFragmentManager.findFragmentByTag("fragment_first_tag") as FragmentFirst
+
+            fragmentFirst.printTestLog()
+        }
         // Commit
         // 1> commit
         // 2> commitAllowingStateLoss
         // 3> commitNow
         // 4> commitNowAllowStateLoss
     }
+
+    fun printTestLog() {
+        Log.d("testt", "print test log")
+    }
+
 }
--- app/src/main/java/com/example/myapplication/FragmentFirst.kt
+++ app/src/main/java/com/example/myapplication/FragmentFirst.kt
@@ -1,9 +1,11 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 
 class FragmentFirst : Fragment() {
@@ -15,7 +17,22 @@ class FragmentFirst : Fragment() {
         // inflater : XML을 화면에 사용할 준비를 한다.
         // container : fragment에서 사용될 XML의 부모뷰
         // attachToRoot : 루트에 붙일지 말지(X), 언제 붙을지
-        return inflater.inflate(R.layout.activity_first_fragment, container, false)
+        val view = inflater.inflate(R.layout.activity_first_fragment, container, false)
+        (view.findViewById<TextView>(R.id.call_activity)).setOnClickListener {
+            (activity as FragmentActivity).printTestLog()
+        }
 
+        return view
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val data: String? = arguments?.getString("key")
+        Log.d("testt", "data is" + data)
+    }
+
+    fun printTestLog() {
+        Log.d("testt", "print test log from fragment")
     }
 }
