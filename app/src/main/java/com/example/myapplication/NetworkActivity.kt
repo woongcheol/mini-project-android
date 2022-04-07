3f9aa130a676e56ebfc11d892cba39b5eafcdb48
Woongcheol Shin <1994_100@naver.com>
2022-04-07 23:30:27 +0900

    add practice(Add networt)

---
+++ app/src/main/java/com/example/myapplication/NetworkActivity.kt
@@ -0,0 +1,44 @@
+package com.example.myapplication
+
+import android.os.AsyncTask
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
+
+class NetworkActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_network)
+
+        NetworkAsyncTask().execute()
+    }
+}
+
+class NetworkAsyncTask() : AsyncTask<Any?, Any?, Any?>() {
+    override fun doInBackground(vararg params: Any?): Any? {
+        val urlString: String = "http://mellowcode.org/json/students/"
+        val url: URL = URL(urlString)
+        val connection: HttpURLConnection = url.openConnection() as HttpURLConnection
+
+        connection.requestMethod = "GET"
+        connection.setRequestProperty(
+            "Content-Type", "application/json"
+        )
+        var buffer = ""
+        if (connection.responseCode == HttpURLConnection.HTTP_OK) {
+            val reader = BufferedReader(
+                InputStreamReader(
+                    connection.inputStream,
+                    "UTF-8"
+                )
+            )
+            buffer = reader.readLine()
+            Log.d("testt",buffer)
+        }
+        return null
+    }
+}
