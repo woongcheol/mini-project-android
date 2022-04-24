3ba13e97d9ee2ebcb87411c6ad9be05b3c52ab07
Woongcheol Shin <1994_100@naver.com>
2022-04-24 10:47:39 +0900

    update project

--- app/src/main/java/com/example/myapplication/DiaryActivity.kt
+++ app/src/main/java/com/example/myapplication/DiaryActivity.kt
@@ -1,11 +1,117 @@
 package com.example.myapplication
 
+import android.content.Context
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.NumberPicker
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.widget.AppCompatButton
+import androidx.core.content.edit
 
 class DiaryActivity : AppCompatActivity() {
+
+    private val numberPicker1: NumberPicker by lazy {
+        findViewById<NumberPicker>(R.id.numberPicker1)
+            .apply {
+                minValue = 0
+                maxValue = 9
+            }
+    }
+
+    private val numberPicker2: NumberPicker by lazy {
+        findViewById<NumberPicker>(R.id.numberPicker2)
+            .apply {
+                minValue = 0
+                maxValue = 9
+            }
+    }
+
+    private val numberPicker3: NumberPicker by lazy {
+        findViewById<NumberPicker>(R.id.numberPicker3)
+            .apply {
+                minValue = 0
+                maxValue = 9
+            }
+    }
+
+    private val openButton: AppCompatButton by lazy {
+        findViewById<AppCompatButton>(R.id.openButton)
+    }
+
+    private val changePasswordButton: AppCompatButton by lazy {
+        findViewById<AppCompatButton>(R.id.changePasswordButton)
+    }
+
+    private var changePasswordMode = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_diary)
+
+        numberPicker1
+        numberPicker2
+        numberPicker3
+
+        openButton.setOnClickListener {
+            if (changePasswordMode) {
+                Toast.makeText(this, "비밀번호 변경 중입니다.", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            val passwordPreference = getSharedPreferences("password", Context.MODE_PRIVATE)
+            val passwordFromUser =
+                "${numberPicker1.value}${numberPicker2.value}${numberPicker3.value}"
+
+            if (passwordPreference.getString("password", "000").equals(passwordFromUser)) {
+
+
+//                startActivity()
+            } else {
+                showErrorAlertDialog()
+            }
+
+
+        }
+
+        changePasswordButton.setOnClickListener {
+            if (changePasswordMode) {
+                val passwordPreference = getSharedPreferences("password", Context.MODE_PRIVATE)
+                val passwordFromUser =
+                    "${numberPicker1.value}${numberPicker2.value}${numberPicker3.value}"
+                passwordPreference.edit {
+                    putString("password", passwordFromUser)
+                    commit()
+                }
+                changePasswordMode = false
+                changePasswordButton.setBackgroundColor(Color.BLACK)
+            } else {
+                val passwordPreference = getSharedPreferences("password", Context.MODE_PRIVATE)
+                val passwordFromUser =
+                    "${numberPicker1.value}${numberPicker2.value}${numberPicker3.value}"
+
+                if (passwordPreference.getString("password", "000").equals(passwordFromUser)) {
+
+                    changePasswordMode = true
+                    Toast.makeText(this, "변경할 패스워드를 입력해주세요", Toast.LENGTH_SHORT).show()
+
+                    changePasswordButton.setBackgroundColor(Color.RED)
+//                startActivity()
+                } else {
+                    showErrorAlertDialog()
+                }
+            }
+        }
+    }
+
+    private fun showErrorAlertDialog() {
+        AlertDialog.Builder(this)
+            .setTitle("실패!!")
+            .setMessage("비밀번호가 잘못되었습니다.")
+            .setPositiveButton("확인") { _, _ ->
+            }
+            .create()
+            .show()
     }
 }
