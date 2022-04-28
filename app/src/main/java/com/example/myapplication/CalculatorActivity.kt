3d4c2c0bad8daacde668229ad2217ea324591928
Woongcheol Shin <1994_100@naver.com>
2022-04-29 03:43:23 +0900

    create project UI(calculator)

---
+++ app/src/main/java/com/example/myapplication/CalculatorActivity.kt
@@ -0,0 +1,26 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class CalculatorActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calculator2)
+    }
+
+    fun buttonClicked(v: View) {
+
+    }
+
+    fun resultButtonClicked(v: View) {
+
+    }
+    fun clearButtonClicked(v: View) {
+
+    }
+    fun historyButtonClicked(v: View) {
+
+    }
+}
